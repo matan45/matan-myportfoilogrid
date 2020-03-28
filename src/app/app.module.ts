1a3558b4c32edb14657b4dd3f6fda7b9d0b60894
matan <matan33214@gmail.com>
2020-03-28 10:50:59 +0300

    profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { MyProjectsComponent } from './components/my-projects/my-projects.component';
+
+const routes: Routes = [
+  {path: 'about-me',component: AboutMeComponent},
+  {path: 'my-projects',component: MyProjectsComponent},
+  {path: 'resume',component: ResumeComponent},
+  {path: '**',redirectTo: "/about-me"}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    ResumeComponent,
+    MyProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
